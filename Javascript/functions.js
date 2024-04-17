06c8cd261b97bc1d9cf181d2d0ef50de869769bb
mayurpatil96k <mayur1792001@gmail.com>
2024-04-17 18:03:55 +0530

    [Mayur]Add: functions info added.

---
+++ Javascript/functions.js
@@ -0,0 +1,20 @@
+// Regular Functions
+function square(x){
+    return x*x;
+}
+
+function greet(){
+    console.log("Hello There Aur ye hum agaye frontend me...")
+}
+
+
+// Arrow Functions
+let ArrowSquare  =(x) => x*x;
+
+// Anonymous/Noname Functions
+setTimeout(function(){
+    console.log("demo hai bhai ye")
+},2000);
+
+
+
