2804dcac001f993de47b77c0c29ed3ed11c828bb
mayurpatil96k <mayur1792001@gmail.com>
2024-04-18 17:41:00 +0530

    [Mayur]Add: Adding use case 7

---
+++ EmployeeWage/usecase7.js
@@ -0,0 +1,64 @@
+class employee{
+    constructor(){}
+    attendance(prab){
+        if (prab <= 0) {
+            return "Present";
+        } else {
+            return "Absent";
+        }
+    }
+    dailywage(wage,hour){
+        return wage*hour;
+    }
+    isparttime(test){
+        if (test <= 0) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+    
+    parttimewage(wage,hour){
+        return wage*hour;
+    }
+    ispresent(num){
+        switch (num) {
+            case 0:
+                return "Absent";
+                break;
+            case 1:
+                return "Present";
+                break;
+        
+            default:
+                return "wrong choice"
+                break;
+        }
+    }
+    
+    isparttime(choice){
+        switch(choice){
+            case 0:
+                return "true"
+                break;
+            case 1:
+                return "false"
+                break;
+            default:
+                return "Invalid input"
+                break;
+        }
+    }
+    monthlyWage(wage,days){
+        return wage*days;
+    }
+    wagecal(wage,days,hours){
+        var final=0;
+        while(days<=20 || hours <=160){
+            final=final+hours*wage;
+            days++;
+            hours++;
+        }
+        return final;
+    }
+}
