e03498b2570107a3681b03ee3be4e09df3ed2443
mayurpatil96k <mayur1792001@gmail.com>
2024-04-18 15:49:40 +0530

    [Mayur]Add: Adding Use Case 1

--- EmployeeWage/Employee.js
+++ EmployeeWage/Employee.js
@@ -1,1 +1,13 @@
-console.log("Welcome to Employee Wage Computation Program on Master Branch")
+console.log("Welcome to Employee Wage Computation Program on Master Branch");
+const pr = Math.floor(Math.random() * 2);
+// const pr = 0
+
+function attendance(prab){
+    if (prab <= 0) {
+        return "Present";
+    } else {
+        return "Absent";
+    }
+}
+
+console.log(attendance(pr));
