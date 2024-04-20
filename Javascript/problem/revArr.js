3e9351d3e464efe3ed8282d7d24c9401d723f03c
mayurpatil96k <mayur1792001@gmail.com>
2024-04-20 14:32:35 +0530

    [Mayur]Fix: Fixed some files

--- Javascript/problem/revArr.js
+++ Javascript/problem/revArr.js
@@ -1,1 +1,16 @@
-// reversse array
+function reverseArray(arr,start,end) 
+{ 
+    while (start < end) 
+    { 
+        var temp = arr[start]; 
+        arr[start] = arr[end]; 
+        arr[end] = temp; 
+        start++; 
+        end--; 
+    } 
+    console.log(arr)
+} 
+
+
+const arr2=[1,2,3,4]
+reverseArray(arr2,0,3);
